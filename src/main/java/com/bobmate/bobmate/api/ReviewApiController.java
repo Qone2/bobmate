6f6b7039b429478f622f325e6b445bbe88cebfb3
qone2 <whrbdnjs33@naver.com>
2021-12-17 02:28:11 +0900

    리뷰api컨트롤러 조회기능 추가

--- src/main/java/com/bobmate/bobmate/api/ReviewApiController.java
+++ src/main/java/com/bobmate/bobmate/api/ReviewApiController.java
@@ -1,18 +1,21 @@
 package com.bobmate.bobmate.api;
 
+import com.bobmate.bobmate.domain.Review;
+import com.bobmate.bobmate.domain.ReviewStatus;
 import com.bobmate.bobmate.service.ReviewService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -44,5 +47,49 @@ public class ReviewApiController {
     @AllArgsConstructor
     static class CreateReviewResponse {
         private Long reviewId;
+    }
+
+    @GetMapping("/api/v1/review")
+    public Result reviewsV1() {
+        List<Review> reviews = reviewService.findAll();
+        List<ReviewDto> collect = reviews.stream()
+                .map(r -> new ReviewDto(r.getId()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class ReviewDto {
+        private Long review_id;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @GetMapping("/api/v1/review/{id}")
+    public ReviewDetailResponse reviewDetailV1(@PathVariable("id") Long id) {
+        Review review = reviewService.findOne(id);
+        return new ReviewDetailResponse(review.getId(), review.getMember().getId()
+                , review.getPlace().getId(), review.getContents(), review.getStar(), review.getCreatedDate()
+                , review.getUpdatedDate(), review.getLikeCount(), review.getReviewStatus());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class ReviewDetailResponse {
+        private Long review_id;
+        private Long member_id;
+        private Long place_id;
+        private String content;
+        private Double star;
+        private LocalDateTime created_date;
+        private LocalDateTime updated_date;
+        private int like_count;
+        private ReviewStatus review_status;
     }
 }
--- src/main/java/com/bobmate/bobmate/service/ReviewService.java
+++ src/main/java/com/bobmate/bobmate/service/ReviewService.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -52,5 +54,9 @@ public class ReviewService {
         review.setStar(star);
 
         return review.getId();
+    }
+
+    public List<Review> findAll() {
+        return reviewRepository.findAll();
     }
 }
