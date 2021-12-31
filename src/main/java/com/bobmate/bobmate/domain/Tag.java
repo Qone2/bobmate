1501b71cdb4afaee02650073875d6deb22626359
qone2 <whrbdnjs33@naver.com>
2022-01-01 00:19:36 +0900

    tag 도메인 추가

---
+++ src/main/java/com/bobmate/bobmate/domain/Tag.java
@@ -0,0 +1,22 @@
+package com.bobmate.bobmate.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Tag {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "tag_id")
+    private Long id;
+
+    private String name;
+
+}
