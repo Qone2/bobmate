a28c238e7a39cdf8b741f6e0197ebec694151422
qone2 <whrbdnjs33@naver.com>
2021-12-31 12:57:52 +0900

    bookmark 서비스 추가

---
+++ src/main/java/com/bobmate/bobmate/service/BookmarkService.java
@@ -0,0 +1,42 @@
+package com.bobmate.bobmate.service;
+
+import com.bobmate.bobmate.domain.Bookmark;
+import com.bobmate.bobmate.domain.Member;
+import com.bobmate.bobmate.domain.Place;
+import com.bobmate.bobmate.repository.BookmarkRepository;
+import com.bobmate.bobmate.repository.MemberRepository;
+import com.bobmate.bobmate.repository.PlaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BookmarkService {
+
+    private final BookmarkRepository bookmarkRepository;
+    private final MemberRepository memberRepository;
+    private final PlaceRepository placeRepository;
+
+    @Transactional
+    public Long saveBookmark(Long memberId, Long placeId) {
+        Member member = memberRepository.findOne(memberId);
+        Place place = placeRepository.findOne(placeId);
+
+        Bookmark bookmark = Bookmark.createBookmark(member, place);
+
+        bookmarkRepository.save(bookmark);
+        return bookmark.getId();
+    }
+
+    @Transactional
+    public void deleteBookmark(Long memberId, Long placeId) {
+        Member member = memberRepository.findOne(memberId);
+        Place place = placeRepository.findOne(placeId);
+
+        Bookmark bookmark = bookmarkRepository.findOneByMemberIdAndPlaceId(member, place);
+
+        bookmarkRepository.delete(bookmark);
+    }
+}
