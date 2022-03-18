5b18f2068ff2feb546c22e4ba89ca4ecc8bb22ef
qone2 <whrbdnjs33@naver.com>
2022-03-18 19:40:04 +0900

    태그 서비스 태그된 횟수 내림차순 조회 추가
    태그된 횟수 순으로(내림차순) 태그아이디와 태그횟수를 map entry 형태로 반환한다.(List<Map.Entry<Long, Integer>>)

--- src/main/java/com/bobmate/bobmate/service/TagService.java
+++ src/main/java/com/bobmate/bobmate/service/TagService.java
@@ -1,12 +1,14 @@
 package com.bobmate.bobmate.service;
 
 import com.bobmate.bobmate.domain.Tag;
+import com.bobmate.bobmate.domain.TagBookmark;
+import com.bobmate.bobmate.repository.TagBookmarkRepository;
 import com.bobmate.bobmate.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +16,7 @@ import java.util.List;
 public class TagService {
 
     private final TagRepository tagRepository;
+    private final TagBookmarkRepository tagBookmarkRepository;
 
 
     /**
@@ -49,5 +52,31 @@ public class TagService {
      */
     public Tag findOne(Long id) {
         return tagRepository.findOne(id);
+    }
+
+    /**
+     * 태그된 개수별 내림차순 조회
+     */
+    public List<Map.Entry<Long, Integer>> findAllByTaggedCount() {
+        List<TagBookmark> tagBookmarkList = tagBookmarkRepository.findAll();
+        HashMap<Long, Integer> map = new HashMap<>();
+        for (TagBookmark tagBookmark : tagBookmarkList) {
+            Long id = tagBookmark.getTag().getId();
+            if (map.containsKey(id)) {
+                map.put(id, 1);
+            } else {
+                map.put(id, map.get(id) + 1);
+            }
+        }
+
+        List<Map.Entry<Long, Integer>> entryList = new ArrayList<>(map.entrySet());
+        entryList.sort(new Comparator<Map.Entry<Long, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Long, Integer> o1, Map.Entry<Long, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
+
+        return entryList;
     }
 }
