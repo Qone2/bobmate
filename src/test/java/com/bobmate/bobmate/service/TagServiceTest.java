7f5fdc11407c1018d77a11c0e14123689f184673
qone2 <whrbdnjs33@naver.com>
2022-03-22 22:52:45 +0900

    태그서비스 테스트 추가

---
+++ src/test/java/com/bobmate/bobmate/service/TagServiceTest.java
@@ -0,0 +1,23 @@
+package com.bobmate.bobmate.service;
+
+import com.bobmate.bobmate.domain.Coordinate;
+import com.bobmate.bobmate.domain.Member;
+import com.bobmate.bobmate.domain.Place;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class TagServiceTest {
+
+    
+}
