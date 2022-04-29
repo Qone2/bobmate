694efd5d9b22d97151d0944d8dffd5bd3e379bb1
qone2 <whrbdnjs33@naver.com>
2022-04-29 23:45:20 +0900

    예외 추가
    삭제된 멤버에 대한 예외

---
+++ src/main/java/com/bobmate/bobmate/exception/DeletedMemberException.java
@@ -0,0 +1,23 @@
+package com.bobmate.bobmate.exception;
+
+public class DeletedMemberException extends RuntimeException{
+    public DeletedMemberException() {
+        super();
+    }
+
+    public DeletedMemberException(String message) {
+        super(message);
+    }
+
+    public DeletedMemberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DeletedMemberException(Throwable cause) {
+        super(cause);
+    }
+
+    protected DeletedMemberException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
