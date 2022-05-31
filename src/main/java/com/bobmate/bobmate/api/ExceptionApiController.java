79c106174ae5971d0cf6b9acd451a3bfd4165be1
qone2 <whrbdnjs33@naver.com>
2022-05-31 20:32:47 +0900

    예외컨트롤러 수정

--- src/main/java/com/bobmate/bobmate/api/ExceptionApiController.java
+++ src/main/java/com/bobmate/bobmate/api/ExceptionApiController.java
@@ -1,22 +1,59 @@
 package com.bobmate.bobmate.api;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionApiController {
 
-    public ResponseEntity badRequest(final RuntimeException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+    @ExceptionHandler({
+            RuntimeException.class
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity badRequest(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(
+                new BadRequestBody("code", "잘못된 접근 입니다.")
+        );
     }
 
-
-    public ResponseEntity accessDenied(final RuntimeException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    @AllArgsConstructor
+    @Getter
+    static class BadRequestBody {
+        private String code;
+        private String message;
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity accessDenied(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(
+                new AccessDeniedBody("401", "Could not validate credentials")
+        );
+
+    }
+
+    @AllArgsConstructor
+    @Getter
     static class AccessDeniedBody {
+        private String code;
+        private String message;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity serverError(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                new ServerErrorBody("500", "Internal server error")
+        );
+    }
+
+    @AllArgsConstructor
+    @Getter
+    static class ServerErrorBody {
         private String code;
         private String message;
     }
