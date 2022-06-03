4d997d1a59e45ec71e09eee38420057f60f08a47
qone2 <whrbdnjs33@naver.com>
2022-06-03 23:00:46 +0900

    리뷰좋아요 컨트롤러 swagger 수정

--- src/main/java/com/bobmate/bobmate/api/LikeReviewApiController.java
+++ src/main/java/com/bobmate/bobmate/api/LikeReviewApiController.java
@@ -4,7 +4,8 @@ import com.bobmate.bobmate.domain.LikeReview;
 import com.bobmate.bobmate.service.LikeReviewService;
 import com.bobmate.bobmate.service.MemberService;
 import com.bobmate.bobmate.service.ReviewService;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -33,7 +34,11 @@ public class LikeReviewApiController {
      */
     @PostMapping("/api/v1/like-review")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation(value = "리뷰좋아요")
+    @Operation(summary = "리뷰좋아요", description = "리뷰에 좋아요를 합니다. 좋아요를 수행하는 멤버와 장소가 명시되어야합니다.<br><br>" +
+            "발생가능한 예외:<br>" +
+            "400 : 이미 좋아요 되어있는 경우, 삭제된 리뷰인 경우<br>" +
+            "404 : 요청한 자원을 찾을 수 없는 경우<br>" +
+            "500 : 내부 서버 에러")
     public LikeReviewResponse likeReviewV1(@RequestBody @Valid LikeReviewRequest request) {
         Long likeReviewId =  likeReviewService.likeReview(request.getMember_id(), request.getReview_id());
         return new LikeReviewResponse(likeReviewId);
@@ -42,14 +47,17 @@ public class LikeReviewApiController {
     @Data
     @AllArgsConstructor
     static class LikeReviewResponse {
+        @Schema(description = "생선된 좋아요id")
         private Long like_review_id;
     }
 
     @Data
     static class LikeReviewRequest {
         @NotNull
+        @Schema(description = "좋아요를 신청하는 멤버id", required = true)
         private Long member_id;
         @NotNull
+        @Schema(description = "좋아요의 대상이 되는 리뷰", required = true)
         private Long review_id;
     }
 
@@ -58,7 +66,11 @@ public class LikeReviewApiController {
      * 리뷰 좋아요 취소
      */
     @DeleteMapping("/api/v1/like-review")
-    @ApiOperation(value = "리뷰 좋아요 취소")
+    @Operation(summary = "리뷰 좋아요 취소", description = "리뷰에 좋아요 한것을 취소(삭제)합니다. 좋아요를 수행한 멤버와, 리뷰의 " +
+            "id가 명시되어야 합니다.<br><br>" +
+            "발생가능한 예외:<br>" +
+            "404 : 요청한 자원을 찾을 수 없는 경우<br>" +
+            "500 : 내부 서버 에러")
     public UnlikeReviewResponse unlikeReviewV1(@RequestBody @Valid UnlikeReviewRequest request) {
         likeReviewService.unlikeReview(request.getMember_id(), request.getReview_id());
         return new UnlikeReviewResponse("done!");
@@ -67,14 +79,17 @@ public class LikeReviewApiController {
     @Data
     @AllArgsConstructor
     static class UnlikeReviewResponse {
+        @Schema(description = "수행결과 메시지")
         private String massage;
     }
 
     @Data
     static class UnlikeReviewRequest {
         @NotNull
+        @Schema(description = "좋아요한 멤버id")
         private Long member_id;
         @NotNull
+        @Schema(description = "좋아요 되어있는 리뷰id")
         private Long review_id;
     }
 
@@ -83,7 +98,10 @@ public class LikeReviewApiController {
      * 리뷰좋아요 전체 조회
      */
     @GetMapping("/api/v1/like-review")
-    @ApiOperation(value = "리뷰좋아요 전체 조회")
+    @Operation(summary = "리뷰좋아요 전체 조회", description = "리뷰에 좋아요한 정보를 전체조회합니다.<br><br>" +
+            "발생가능한 예외:<br>" +
+            "404 : 요청한 자원을 찾을 수 없는 경우<br>" +
+            "500 : 내부 서버 에러")
     public Result likeReviewsV1() {
         List<LikeReview> likeReviewList = likeReviewService.findAll();
         List<LikeReviewDto> likeReviewDtoList = likeReviewList.stream()
@@ -113,7 +131,11 @@ public class LikeReviewApiController {
      * 리뷰좋아요 상세 조회
      */
     @GetMapping("/api/v1/like-review/{id}")
-    @ApiOperation(value = "리뷰좋아요 상세 조회")
+    @Operation(summary = "리뷰좋아요 상세 조회", description = "리뷰에 좋아요한 정보를 상세조회합니다. schema버튼을 누르면 " +
+            "상세정보 제공.<br><br>" +
+            "발생가능한 예외:<br>" +
+            "404 : 요청한 자원을 찾을 수 없는 경우<br>" +
+            "500 : 내부 서버 에러")
     public LikeReviewDetailResponse likeReviewDetailV1(@PathVariable Long id) {
         LikeReview findLikeReview = likeReviewService.findOne(id);
         return new LikeReviewDetailResponse(findLikeReview.getId(), findLikeReview.getMember().getId(),
@@ -123,9 +145,13 @@ public class LikeReviewApiController {
     @Data
     @AllArgsConstructor
     static class LikeReviewDetailResponse {
+        @Schema(description = "리뷰좋아요id")
         private Long like_review_id;
+        @Schema(description = "좋아요한 멤버의 id")
         private Long member_id;
+        @Schema(description = "좋아요 당한 리뷰의 id")
         private Long review_id;
+        @Schema(description = "좋아요 한 날짜")
         private LocalDateTime date;
     }
 }
