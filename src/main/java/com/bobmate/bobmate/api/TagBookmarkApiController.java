78bb5fc5da2008eba3e8eedfc4ba640e9a27ccee
qone2 <whrbdnjs33@naver.com>
2022-06-04 18:40:04 +0900

    태그 북마크 컨트롤러 swagger 수정

--- src/main/java/com/bobmate/bobmate/api/TagBookmarkApiController.java
+++ src/main/java/com/bobmate/bobmate/api/TagBookmarkApiController.java
@@ -2,7 +2,8 @@ package com.bobmate.bobmate.api;
 
 import com.bobmate.bobmate.domain.Bookmark;
 import com.bobmate.bobmate.service.TagBookmarkService;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -28,25 +29,33 @@ public class TagBookmarkApiController {
      */
     @PostMapping("/api/v1/tagbookmark")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation("북마크 태그 설정")
+    @Operation(summary = "북마크 태그 설정", description = "북마크에 태그를 등록합니다.<br>" +
+            "멤버id는 검증용으로 북마크의 소유가 일치하는지 확인합니다. 필요없는 부분이라고 판단되면 알려주시기 바랍니다.<br><br>" +
+            "발생가능한 예외:<br>" +
+            "400 : 멤버가 북마크의 소유자가 아닌경우, 북마크에 태그가 이미 등록되어있는 경우<br>" +
+            "404 : 요청한 자원을 찾을 수 없는 경우<br>" +
+            "500 : 내부 서버 에러")
     public CreateTagBookmarkResponse createTagBookmarkV1(@RequestBody @Valid CreateTagBookmarkRequest request) {
-        Long tagBookmarkId = tagBookmarkService.saveTagBookmark(request.getTag_id(), request.getBookmark_id(), request.getMember_id());
-        return new CreateTagBookmarkResponse(tagBookmarkId);
+        tagBookmarkService.saveTagBookmark(request.getTag_id(), request.getBookmark_id(), request.getMember_id());
+        return new CreateTagBookmarkResponse("success");
     }
 
     @Data
     @AllArgsConstructor
     static class CreateTagBookmarkResponse {
-        private Long tag_bookmark_id;
+        private String message;
     }
 
     @Data
     static class CreateTagBookmarkRequest {
         @NotNull
+        @Schema(description = "검증용 멤버id(북마크의 소유자)", required = true)
         private Long member_id;
         @NotNull
+        @Schema(description = "태그될 북마크id", required = true)
         private Long bookmark_id;
         @NotNull
+        @Schema(description = "태그할 태그id", required = true)
         private Long tag_id;
     }
 
@@ -55,9 +64,14 @@ public class TagBookmarkApiController {
      * 해당 태그(들)을 설정한 북마크 전체 조회
      */
     @GetMapping("/api/v1/tagbookmark")
-    @ApiOperation("해당 태그(들)을 설정한 북마크 전체 조회")
+    @Operation(summary = "해당 태그(들)을 설정한 북마크 전체 조회", description = "태그id들을 리스트로 만들어 요청을 보내면" +
+            "해당 태그에 해당하는 북마크id 리스트를 리턴합니다. and연산으로 처리됩니다. 북마크 소유주 멤버id가 명시되어야 합니다.<br><br>" +
+            "발생가능한 예외:<br>" +
+            "404 : 요청한 자원을 찾을 수 없는 경우<br>" +
+            "500 : 내부 서버 에러")
     public GetTagBookmarkResponse taggedBookmarksV1(@RequestBody @Valid GetTagBookmarkRequest request) {
-        List<Bookmark> taggedBookmark = tagBookmarkService.findTaggedBookmark(request.getMember_id(), request.getTag_id_list());
+        List<Bookmark> taggedBookmark = tagBookmarkService.findTaggedBookmark(
+                request.getMember_id(), request.getTag_id_list());
 
         return new GetTagBookmarkResponse(request.getMember_id(), request.getTag_id_list(),
                 taggedBookmark.stream().map(tb -> tb.getId()).collect(Collectors.toList()));
@@ -66,16 +80,21 @@ public class TagBookmarkApiController {
     @Data
     @AllArgsConstructor
     static class GetTagBookmarkResponse {
+        @Schema(description = "요청할때 명시한 멤버id")
         private Long member_id;
+        @Schema(description = "요청할때 사용한 태그 id 리스트")
         private List<Long> tag_ids;
+        @Schema(description = "요청결과로 나오는 북마크id 리스트")
         private List<Long> tagged_bookmark_id_list;
     }
 
     @Data
     static class GetTagBookmarkRequest {
         @NotNull
+        @Schema(description = "불러올 북마크의 소유주 멤버 id", required = true)
         private Long member_id;
         @NotNull
+        @Schema(description = "적용할 태그 id 리스트", required = true)
         private List<Long> tag_id_list;
     }
 
@@ -84,10 +103,15 @@ public class TagBookmarkApiController {
      * 북마크 태그 설정 취소
      */
     @DeleteMapping("/api/v1/tagbookmark")
-    @ApiOperation("북마크 태그 설정 취소")
+    @Operation(summary = "북마크 태그 설정 취소", description = "북마크에 적용된 태그를 취소 합니다.<br>" +
+            "요청하는데 필요한 멤버id는 북마크의 소유주가 맞는지 검증하는 용도 입니다.<br><br>" +
+            "발생가능한 예외:<br>" +
+            "400 : 요청한 멤버가 북마크의 소유주가 아닌경우<br>" +
+            "404 : 요청한 자원을 찾을 수 없는 경우<br>" +
+            "500 : 내부 서버 에러")
     public DeleteTagBookmarkResponse deleteTagBookmarkV1(@RequestBody @Valid DeleteTagBookmarkRequest request) {
         tagBookmarkService.deleteTagBookmark(request.getTag_id(), request.getBookmark_id(), request.getMember_id());
-        return new DeleteTagBookmarkResponse("태그 취소됨");
+        return new DeleteTagBookmarkResponse("success");
     }
 
     @Data
@@ -99,10 +123,13 @@ public class TagBookmarkApiController {
     @Data
     static class DeleteTagBookmarkRequest {
         @NotNull
+        @Schema(description = "북마크 검증용 멤버id", required = true)
         private Long member_id;
         @NotNull
+        @Schema(description = "태그 해제될 북마크 id", required = true)
         private Long bookmark_id;
         @NotNull
+        @Schema(description = "해제할 태그id", required = true)
         private Long tag_id;
     }
 }
