8eaea7799ae31665ba447ad3e62757b5c58df21f
qone2 <whrbdnjs33@naver.com>
2022-06-15 00:21:06 +0900

    hotfix

--- src/main/java/com/bobmate/bobmate/api/MemberApiController.java
+++ src/main/java/com/bobmate/bobmate/api/MemberApiController.java
@@ -18,6 +18,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.Collections;
