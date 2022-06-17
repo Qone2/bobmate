06e66bf414caba04f9cc95eae75ec78a34f1e9be
qone2 <whrbdnjs33@naver.com>
2022-06-17 20:44:08 +0900

    open api 설정 변경

--- src/main/java/com/bobmate/bobmate/config/SwaggerConfig.java
+++ src/main/java/com/bobmate/bobmate/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
@@ -13,6 +14,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import java.util.Collections;
 
 @Configuration
+@EnableOpenApi
 public class SwaggerConfig {
 
     @Bean
