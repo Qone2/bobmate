9ff6bd584b755f759c5a970fc01ee618047b3265
Qone2 <whrbdnjs33@naver.com>
2022-07-08 23:39:50 +0900

    사진 서비스 사진전체조회 추가

--- src/main/java/com/bobmate/bobmate/service/PhotoService.java
+++ src/main/java/com/bobmate/bobmate/service/PhotoService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -14,7 +16,17 @@ public class PhotoService {
     private final PhotoRepository photoRepository;
 
 
+    /**
+     * 사진 단일 조회
+     */
     public Photo findOne(Long photoId) {
         return photoRepository.findOne(photoId);
+    }
+
+    /**
+     * 사진 전체 조회
+     */
+    public List<Photo> findAll() {
+        return photoRepository.findAll();
     }
 }
